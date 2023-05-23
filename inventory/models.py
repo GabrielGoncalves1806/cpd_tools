2675557e5b16689a18441fb0e0ed30466a2d01cf
Gabriel Oliveira Gonçalves <97641390+GabrielGoncalves1806@users.noreply.github.com>
2023-05-23 00:53:58 -0300

    create a model resale and state

--- inventory/models.py
+++ inventory/models.py
@@ -2,19 +2,29 @@ from django.db import models
 
 # Create your models here.
 class ItemGroup(models.Model):
-    group_name = models.CharField(max_length=255)
-
+    group_name = models.CharField(max_length=30)
     def __str__(self):
         return self.group_name
 
 class Brand(models.Model):
-    brand_name = models.CharField(max_length=255)
-
+    brand_name = models.CharField(max_length=30)
     def __str__(self):
         return self.brand_name
+    
+class State(models.Model):
+    state_name = models.CharField(max_length=30)
+    def __str__(self):
+        return self.state_name
 
 class Item(models.Model):
+    item_name = models.CharField(max_length=50)
+    item_sn = models.CharField(max_length=50)
     item_group = models.ForeignKey(ItemGroup, on_delete=models.CASCADE)
-    item_name = models.CharField(max_length=255)
     item_brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    item_sn = models.CharField(max_length=255)
+    item_state = models.ForeignKey(State, on_delete=models.CASCADE)
+    
+class Resale(models.Model):
+    resale_name = models.CharField(max_length=50)
+    resale_itens = models.ManyToManyField(Item)
+    def __str__(self):
+        return self.resale_name
