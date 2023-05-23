8e907b0b4c9ca4bdc43c778b95ef82a82ef604bc
Gabriel Oliveira Gonçalves <97641390+GabrielGoncalves1806@users.noreply.github.com>
2023-05-23 01:54:24 -0300

    update state model

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
-from .models import Item, ItemGroup, Brand
+from .models import Item, ItemGroup, Brand, State
 
 # Register your models here.
 @admin.register(Item)
 class ItemAdmin(admin.ModelAdmin):
-    list_display = ['id', 'item_name', 'item_brand', 'item_sn','item_group']
+    list_display = ['id', 'item_name', 'item_sn', 'item_brand', 'item_group', 'item_state']
 
 @admin.register(ItemGroup)
 class ItemGroupAdmin(admin.ModelAdmin):
@@ -13,3 +13,7 @@ class ItemGroupAdmin(admin.ModelAdmin):
 @admin.register(Brand)
 class BrandAdmin(admin.ModelAdmin):
     list_display = ['id','brand_name']
+
+@admin.register(State)
+class StateAdmin(admin.ModelAdmin):
+    list_display = ['state_name']
--- inventory/models.py
+++ inventory/models.py
@@ -15,16 +15,15 @@ class State(models.Model):
     state_name = models.CharField(max_length=30)
     def __str__(self):
         return self.state_name
-
+    
 class Item(models.Model):
     item_name = models.CharField(max_length=50)
     item_sn = models.CharField(max_length=50)
-    item_group = models.ForeignKey(ItemGroup, on_delete=models.CASCADE)
-    item_brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
-    item_state = models.ForeignKey(State, on_delete=models.CASCADE)
+    item_brand = models.ForeignKey(Brand, on_delete=models.CASCADE, null=True)
+    item_group = models.ForeignKey(ItemGroup, on_delete=models.CASCADE, null=True)
+    item_state = models.ForeignKey(State,on_delete=models.CASCADE, null=True)
     
-class Resale(models.Model):
-    resale_name = models.CharField(max_length=50)
-    resale_itens = models.ManyToManyField(Item)
-    def __str__(self):
-        return self.resale_name
+    
+    
+    
+    
